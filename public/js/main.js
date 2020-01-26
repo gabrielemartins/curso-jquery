6ce1a2a45cff225ae1277109b758849a02122649
gabriele <gabriieele6@gmail.com>
2020-01-26 14:00:11 -0300

    Contando caracteres e palavras na digitação

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,15 @@ var frase = $('.frase').text();
 var numPalavras = frase.split(' ').length;
 var tamanhoFrase = $('.numPalavras');
 tamanhoFrase.text(numPalavras);
+
+var campoTexto = $('.campoDigitacao');
+campoTexto.on('input', function () {
+    var conteudo = campoTexto.val();
+
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    var contPalavras = $('.contPalavras');
+    contPalavras.text(qtdPalavras)
+
+    var contCaracteres = $('.contCaracteres');
+    contCaracteres.text(conteudo.length)
+})
