9b22deaae4682a998d22876fb44672b0f228e00a
gabriele <gabriieele6@gmail.com>
2020-01-26 18:23:05 -0300

    Implementando botão reiniciar e melhorando o código

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,56 @@
-var frase = $('.frase').text();
-var numPalavras = frase.split(' ').length;
-var tamanhoFrase = $('.numPalavras');
-tamanhoFrase.text(numPalavras);
-
+var botaoReinicio = $('.reinicioJogo');
+var tempoInicial = $('.segundos').text();
 var campoTexto = $('.campoDigitacao');
-campoTexto.on('input', function () {
-    var conteudo = campoTexto.val();
 
-    var qtdPalavras = conteudo.split(/\S+/).length - 1;
-    var contPalavras = $('.contPalavras');
-    contPalavras.text(qtdPalavras)
-
-    var contCaracteres = $('.contCaracteres');
-    contCaracteres.text(conteudo.length);
+$(function () {
+    tamanhoFrase();
+    contadores();
+    cronometro();
+    botaoReinicio.click(reiniciarJogo);
 });
 
-var tempo = $('.segundos').text();
-campoTexto.one('focus', function () {
-    var cronometroId = setInterval(function () {
-        tempo--;
-        $('.segundos').text(tempo)
-        if (tempo <=0) {
-          campoTexto.attr('disabled', true);
-          clearInterval(cronometroId);
-        }
-      }, 1000);
-});
+function tamanhoFrase() {
+    var frase = $('.frase').text();
+    var numPalavras = frase.split(' ').length;
+    var tamanhoFrase = $('.numPalavras');
+    tamanhoFrase.text(numPalavras);
+}
+
+function contadores () {
+    campoTexto.on('input', function () {
+        var conteudo = campoTexto.val();
+    
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        var contPalavras = $('.contPalavras');
+        contPalavras.text(qtdPalavras)
+    
+        var contCaracteres = $('.contCaracteres');
+        contCaracteres.text(conteudo.length);
+    });
+    
+}
+
+function cronometro() {
+    var tempo = $('.segundos').text();
+    campoTexto.one('focus', function () {
+        botaoReinicio.attr("disabled",true);
+        var cronometroId = setInterval(function () {
+            tempo--;
+            $('.segundos').text(tempo)
+            if (tempo <=0) {
+              campoTexto.attr('disabled', true);
+              clearInterval(cronometroId);
+              botaoReinicio.attr("disabled", false);
+            }
+          }, 1000);
+    });
+}
+
+function reiniciarJogo() {
+        campoTexto.attr('disabled', false);
+        campoTexto.val('');
+        $('.contPalavras').text('0');
+        $('.contCaracteres').text('0');
+        $('.segundos').text(tempoInicial);
+        cronometro();
+}
