d4023a60fbee8283f0ce54242062c1d140e560fd
gabriele <gabriieele6@gmail.com>
2020-01-26 23:15:09 -0300

    Implementando botão para remover linha no placar

--- public/js/main.js
+++ public/js/main.js
@@ -93,9 +93,33 @@ function placar() {
     var tabela = $('.placar').find('tbody');
     var usuario = 'Gabriele';
     var numPalavras = $('.contPalavras').text();
-    var linha = '<tr>'+
-                   '<td>' + usuario + '</td>' +
-                   '<td>' + numPalavras + '</td>' +
-                '</tr>';
+    var botaoRemover =  "<a href='#' class='remover'><i class='small material-icons'>delete</i></a>" ;
+    var linha = novaLinha(usuario, numPalavras);
+    linha.find('.remover').click(removerLinha)
     tabela.append(linha);
 }
+
+function novaLinha(usuario, numPalavras) {
+    var linha = $('<tr>');
+    var colunaUsuario = $('<td>').text(usuario);
+    var colunaPalavras = $('<td>').text(numPalavras);
+    var colunaRemover = $('<td>');
+    var linkRemover = $('<a>').addClass('remover').attr('href', '#');
+    var icone = $('<i>').addClass('small').addClass('material-icons').text('delete');
+  
+    linkRemover.append(icone);
+    colunaRemover.append(linkRemover);
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+  
+    return linha;
+}
+
+function removerLinha() {
+    event.preventDefault();
+    $(this).parent().parent().remove();
+}
+
+
+
